58c000ef7d52bc2be2661488b33c1e8c7cd16aff
arinavictor <arinavictor@yahoo.com>
2019-12-02 16:43:45 -0700

    first commit and starts to implement authorization for /profile to be able to save user data in state on the frontend.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+
+    def profile
+     render json: { user: current_user }, status: :accepted
+    end 
+    
+    def create
+       @user =  User.new(user_params)
+       if @user.save
+            render json: @user, status: :created
+       else
+            render json: { error: "invalid username" }, status: :bad_request
+       end
+    end 
+
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end 
+end
