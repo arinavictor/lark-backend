4cdfec0e12efa1fa1d2080d1d95c559408e22fdb
arinavictor <arinavictor@yahoo.com>
2019-12-03 21:28:09 -0700

    added /profile route to be able to save user object in the frontend, creates a resource that needs authorization to access

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     def profile
      render json: { user: current_user }, status: :accepted
     end 
-    
+
     def create
        @user =  User.new(user_params)
        if @user.save
